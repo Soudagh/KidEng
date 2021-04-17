eb44bc8d707dee0431a68174665b674b0876e815
Soudagh <fr.kristina@inbox.ru>
2021-04-18 03:09:00 +0700

    delete excess methods

--- app/src/main/java/com/example/myproject/MainDialogFragment.java
+++ app/src/main/java/com/example/myproject/MainDialogFragment.java
@@ -9,10 +9,7 @@ public class MainDialogFragment extends android.app.DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         return builder
-//                .setTitle("Начать сначала")
                 .setView(R.layout.dialog)
-//                .setPositiveButton("OK", null)
-//                .setNegativeButton("Отмена", null)
                 .create();
     }
 }
