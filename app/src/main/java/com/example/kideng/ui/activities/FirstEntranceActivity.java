3a1dc93e0adb5bfb396e97eddfa3deb51080d48a
Soudagh <fr.kristina@inbox.ru>
2021-07-03 15:24:58 +0700

    design changes in FirstEntranceActivity

--- app/src/main/java/com/example/kideng/ui/activities/FirstEntranceActivity.java
+++ app/src/main/java/com/example/kideng/ui/activities/FirstEntranceActivity.java
@@ -1,20 +1,21 @@
 package com.example.kideng.ui.activities;
 
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.kideng.R;
 import com.example.kideng.db.legacy.ThemeDBHelper;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class FirstEntranceActivity extends AppCompatActivity {
     private EditText mName;
-    private EditText mNickname_et;
+    private EditText mNickName;
+    private TextInputLayout mNickLayout;
+    private TextInputLayout mNameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +23,27 @@ public class FirstEntranceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         mName = findViewById(R.id.userName_et);
-        mNickname_et = findViewById(R.id.userNickName_et);
+        mNickName = findViewById(R.id.userNickName_et);
+        mNickLayout = findViewById(R.id.textInputNickLayout);
+        mNameLayout = findViewById(R.id.textInputNameLayout);
     }
 
     public void confirmClick(View view) {
-        ThemeDBHelper dbHelper = new ThemeDBHelper(getApplicationContext());
-        dbHelper.insertUser(String.valueOf(mNickname_et.getText()),String.valueOf(mName.getText()));
-        startActivity(new Intent(this, MainActivity.class));
+        if (String.valueOf(mName.getText()).equals("") || String.valueOf(mNickName.getText()).equals("")) {
+            if (String.valueOf(mName.getText()).equals("")) {
+                mNameLayout.setErrorEnabled(true);
+                mNameLayout.setError("Обязательное поле!");
+            }
+
+            if (String.valueOf(mNickName.getText()).equals("")) {
+                mNickLayout.setErrorEnabled(true);
+                mNickLayout.setError("Обязательное поле!");
+            }
+        } else {
+            ThemeDBHelper dbHelper = new ThemeDBHelper(getApplicationContext());
+            dbHelper.insertUser(String.valueOf(mNickName.getText()),String.valueOf(mName.getText()));
+            startActivity(new Intent(this, MainActivity.class));
+        }
+
     }
 }
