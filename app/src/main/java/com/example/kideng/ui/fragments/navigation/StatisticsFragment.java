5f1383faeded20baf1c6ccda69c0e09a10c9f3a1
Soudagh <fr.kristina@inbox.ru>
2022-07-20 03:10:17 +0700

    add stats

--- app/src/main/java/com/example/kideng/ui/fragments/navigation/StatisticsFragment.java
+++ app/src/main/java/com/example/kideng/ui/fragments/navigation/StatisticsFragment.java
@@ -7,60 +7,47 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.kideng.App;
 import com.example.kideng.R;
+import com.example.kideng.db.AppDatabase;
+import com.example.kideng.db.dao.UserDao;
+import com.example.kideng.db.entities.User;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link StatisticsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class StatisticsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public StatisticsFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment StatisticsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static StatisticsFragment newInstance(String param1, String param2) {
+
+    public static StatisticsFragment newInstance() {
         StatisticsFragment fragment = new StatisticsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_statistics, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_statistics, container, false);
+
+        AppDatabase db = App.getInstance().getDatabase();
+        UserDao userDao = db.userDao();
+        User user = userDao.getUser();
+
+        TextView mTotalGames = view.findViewById(R.id.total_games_tv);
+        TextView mTotalAnswers = view.findViewById(R.id.total_answers_tv);
+        TextView mRightAnswers = view.findViewById(R.id.right_answers_tv);
+        TextView mWrongAnswers = view.findViewById(R.id.wrong_answers_tv);
+
+        mTotalGames.setText(String.valueOf(user.getTotalTrains()));
+        mTotalAnswers.setText(String.valueOf(user.getTotalWords()));
+        mRightAnswers.setText(String.valueOf(user.getRightAnswers()));
+        mWrongAnswers.setText(String.valueOf(user.getWrongAnswers()));
+        return view;
     }
 }
