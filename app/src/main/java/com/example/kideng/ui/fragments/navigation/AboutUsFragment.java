a4786bac415e2a48666d7860e5459e3c5d532706
Soudagh <fr.kristina@inbox.ru>
2022-07-23 04:11:24 +0700

    AboutUsFragment

--- app/src/main/java/com/example/kideng/ui/fragments/navigation/AboutUsFragment.java
+++ app/src/main/java/com/example/kideng/ui/fragments/navigation/AboutUsFragment.java
@@ -4,46 +4,21 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.kideng.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AboutUsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AboutUsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    public AboutUsFragment() {}
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AboutUsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AboutUsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AboutUsFragment newInstance(String param1, String param2) {
+    public static AboutUsFragment newInstance() {
         AboutUsFragment fragment = new AboutUsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -51,16 +26,28 @@ public class AboutUsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about_us, container, false);
+        View view =  inflater.inflate(R.layout.fragment_about_us, container, false);
+        TextView mTgLink = view.findViewById(R.id.tg_link);
+        TextView mDsLink = view.findViewById(R.id.ds_link);
+        TextView mGhLink = view.findViewById(R.id.gh_link);
+        if (mTgLink != null) {
+            mTgLink.setMovementMethod(LinkMovementMethod.getInstance());
+        }
+
+        if (mDsLink != null) {
+            mDsLink.setMovementMethod(LinkMovementMethod.getInstance());
+        }
+
+        if (mGhLink != null) {
+            mGhLink.setMovementMethod(LinkMovementMethod.getInstance());
+        }
+
+        return view;
     }
 }
